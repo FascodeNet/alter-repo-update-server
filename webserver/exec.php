0547265c86850a8e9fd010b2512503646ae9f714
naoko1010hh <naoko561010@gmail.com>
2020-07-23 21:20:51 +0900

    update リポジトリ更新を搭載

--- webserver/exec.php
+++ webserver/exec.php
@@ -1,43 +1,47 @@
 <?php
 
-$cmd = 'ls -l /usr';
-exec($cmd, $opt, $return);
-$opt_str="";
-foreach ($opt as $value) {
-    $opt_str.=$value."\n";
-}
-if ($return===1) {
-    $data=[
-        "status" => "ERROR",
-        "output" => $opt_str
-    ];
-    $data = json_encode($data);
-    $context = stream_context_create(
-        array(
-            'http' => array(
-                'method'=> 'POST',
-                'header'=> 'Content-type: application/json; charset=UTF-8',
-                'content' => $data
-            )
-        )
-    );
+command("/home/naoko/alterlinux-pkgbuilds/get_from_aur.sh");
+command("/home/naoko/alterlinux-repository/scripts/main.sh");
 
-    $responses_json = file_get_contents($argv[1], false, $context);
-}elseif ($return===0) {
-    $data=[
-        "status" => "OK",
-        "output" => $opt_str
-    ];
-    $data = json_encode($data);
-    $context = stream_context_create(
-        array(
-            'http' => array(
-                'method'=> 'POST',
-                'header'=> 'Content-type: application/json; charset=UTF-8',
-                'content' => $data
+function command($cmd){
+    exec($cmd, $opt, $return);
+    $opt_str="";
+    foreach ($opt as $value) {
+        $opt_str.=$value."\n";
+    }
+    if ($return===1) {
+        $data=[
+            "status" => "ERROR",
+            "output" => $cmd."\n".$opt_str
+        ];
+        $data = json_encode($data);
+        $context = stream_context_create(
+            array(
+                'http' => array(
+                    'method'=> 'POST',
+                    'header'=> 'Content-type: application/json; charset=UTF-8',
+                    'content' => $data
+                )
             )
-        )
-    );
+        );
 
-    $responses_json = file_get_contents($argv[1], false, $context);
+        $responses_json = file_get_contents($argv[1], false, $context);
+    }elseif ($return===0) {
+        $data=[
+            "status" => "OK",
+            "output" => $cmd."\n".$opt_str
+        ];
+        $data = json_encode($data);
+        $context = stream_context_create(
+            array(
+                'http' => array(
+                    'method'=> 'POST',
+                    'header'=> 'Content-type: application/json; charset=UTF-8',
+                    'content' => $data
+                )
+            )
+        );
+
+        $responses_json = file_get_contents($argv[1], false, $context);
+    }
 }
